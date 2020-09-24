f2cc22cbae6bb073f777821eff91acff95851e62
LucaGoubelle <luca.goubelle97@gmail.com>
2020-09-24 14:55:34 +0200

    FiveCommit

--- sw.js
+++ sw.js
@@ -1,7 +1,26 @@
-console.log("hello depuis le service worker");
 
+const cacheName = 'veille-techno' + '1.1';
+ 
 self.addEventListener('install', (evt) => {
     console.log(`sw installé à ${new Date().toLocaleTimeString()}`);
+ 
+    const cachePromise = caches.open(cacheName).then(cache => {
+        return cache.addAll([
+            'index.html',
+            'main.js',
+            'style.css',
+            'vendors/bootstrap.min.css',
+            'add_techno.html',
+            'add_techno.js',
+            'contact.html',
+            'contact.js',
+        ])
+        .then(console.log('cache initialisé'))
+        .catch(console.err);
+    });
+ 
+    evt.waitUntil(cachePromise);
+ 
 });
  
 self.addEventListener('activate', (evt) => {
